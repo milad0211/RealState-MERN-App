db0d2f93ac46a39203e10a962db6ba0cc73fee18
MilaD <milad.mashayekhi1@gmail.com>
2024-09-10 14:29:21 +0330

    create a test Api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+
 dotenv.config()
+// import UserRouter from './routes/user.route.js';
 //test
 mongoose.connect(process.env.MONGO)
 .then(()=>{console.log('connected to MongoDB')})
@@ -15,3 +18,8 @@ const app = express();
 app.listen(3000,()=>{
     console.log('Server conected port 3000!!');
 })
+//test api 
+app.use('/api/user/',userRouter)
+// app.get('/ab',(req,res)=>{
+//     res.send('secound test')
+// })
