b705dd8be2f19b95c2ab7df9095e20f5c2f64645
MilaD <milad.mashayekhi1@gmail.com>
2024-09-10 11:01:51 +0330

    create header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,11 @@ import About from './Pages/About';
 import Profile from './Pages/Profile';
 import SignIn from './Pages/Signin';
 import SignUp from './Pages/SignUp';
+import Header from './components/Header';
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='about' element={<About/>}/>
