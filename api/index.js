d1be71f2760ce60504d36ef398673d596f7506a2
MilaD <milad.mashayekhi1@gmail.com>
2024-09-10 12:53:02 +0330

    conect to DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// dotenv.config()
+//test
+// mongoose.connect(process.env.MONGO)
+
+mongoose.connect("mongodb+srv://root:root@mern-estate.yk5ej.mongodb.net/mern-estate?retryWrites=true&w=majority&appName=mern-estate")
+.then(()=>{console.log('connected to MongoDB')})
+.catch((err)=>{
+    console.log(`error darim : ${err}`);
+})
+
+
 const app = express();
 
 app.listen(3000,()=>{
